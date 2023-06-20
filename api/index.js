aae0138cf9da64f1ba9588b936c42c07703290ea
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-06-20 18:19:05 +0300

    Added initial random sector calculations.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,25 @@
 import { getRandomInteger, shuffleArray } from './utils.js';
 export * from './data.js';
 
-const indeces = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ];
+const n = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ];
 
+let i = n.length;
+let combinations = i * (i - 1) / 2;
+
+let pairs = [];
+
+function getDistinctPairs(pairs = []) {
+    let random1 = getRandomInteger(0, 10);
+    let random2 = getRandomInteger(0, 10);
+
+    if (random1 === random2) getDistinctPairs(pairs);
+
+    distinct = pairs.every(([ num1, num2 ]) => (num1 !== random1 && num1 !== random2) || (num2 !== random1 && num2 !== random2));
+
+    if (pairs.length === 0 || distinct) {
+        pairs.push([ random1, random2 ]);
+        return pairs;
+    } else {
+        getDistinctPairs(pairs);
+    }
+}
