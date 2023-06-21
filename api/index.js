2b4071225d9942af472b0f5951d117679c48f1da
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-06-21 10:37:46 +0300

    Imported the getDistinctListItemPairs helper method and tested if the correct output is logged in the console.

--- api/index.js
+++ api/index.js
@@ -1,25 +1,9 @@
-import { getRandomInteger, shuffleArray } from './utils.js';
+import {
+    getDistinctListItemPairs, 
+} from './utils.js';
 export * from './data.js';
 
-const n = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ];
+const sectors = Array.from({ length: 14 }, (_, i) => i);
+let winningSectorsPairs = getDistinctListItemPairs(sectors);
 
-let i = n.length;
-let combinations = i * (i - 1) / 2;
-
-let pairs = [];
-
-function getDistinctPairs(pairs = []) {
-    let random1 = getRandomInteger(0, 10);
-    let random2 = getRandomInteger(0, 10);
-
-    if (random1 === random2) getDistinctPairs(pairs);
-
-    distinct = pairs.every(([ num1, num2 ]) => (num1 !== random1 && num1 !== random2) || (num2 !== random1 && num2 !== random2));
-
-    if (pairs.length === 0 || distinct) {
-        pairs.push([ random1, random2 ]);
-        return pairs;
-    } else {
-        getDistinctPairs(pairs);
-    }
-}
+console.table(winningSectorsPairs);
