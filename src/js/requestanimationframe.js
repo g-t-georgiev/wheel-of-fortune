16a598ea7fb65caa0429a2a0038b041240197d0d
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-06-28 18:02:13 +0300

    Added browser specific API detection for the `cancelAnimationFrame`, as well.

--- src/js/requestanimationframe.js
+++ src/js/requestanimationframe.js
@@ -1,12 +1,12 @@
-export const _requestAnimationFrame = (function (globalThis, postfix) {
-    let prefixes = [ '', 'webkit', 'moz', 'ms' ];
-    prefixes = prefixes.map(prefix => prefix + (prefix.length ? 'R' : 'r'));
+export const _requestAnimationFrame = (function (globalThisObj, postfix) {
+    let name,
+        func,
+        prefixes = [ '', 'webkit', 'moz', 'ms' ].map(prefix => prefix + (prefix.length ? 'R' : 'r'));
 
-    let name, func;
     for (let prefix of prefixes) {
         name = prefix + postfix;
         // console.log(name);
-        func = globalThis[name];
+        func = globalThisObj[name];
         if (func) break;
     }
 
@@ -17,5 +17,25 @@ export const _requestAnimationFrame = (function (globalThis, postfix) {
 
     return func;
 })(globalThis, 'equestAnimationFrame');
+
+export const _cancelAnimationFrame = (function (globalThisObj, postfix) {
+    let name,
+        func,
+        prefixes = [ '', 'moz' ].map(prefix => prefix + (prefix.length ? 'C' : 'c'));
+
+    for (let prefix of prefixes) {
+        name = prefix + postfix;
+        // console.log(name);
+        func = globalThisObj[name];
+        if (func) break;
+    }
+
+    // Fallback to a `clearTimeout`
+    if (func == null) {
+        func = function cancelAnimationFrameFallback(timerId) { clearTimeout(timerId) };
+    }
+    
+    return func;
+})(globalThis, 'ancelAnimationFrame')
 
 // console.log(_requestAnimationFrame);
