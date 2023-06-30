b311ef71e21134e141fb6bf55c7adfde5dd13002
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-06-30 16:14:47 +0300

    Get wheel component rendering data and initialize it.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
-import * as api from '../../api/index.js';
 import { WheelComponent } from './wheel.js';
+import { getUIRenderData } from './wheel.service.js';
 
 const rootElementRef = document.querySelector('.app-wrapper');
 
-const wheel = new WheelComponent(rootElementRef, api.data.length, {});
-wheel.initialize(api.data);
+getUIRenderData().subscribe((error, data) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log(data);
+    const wheel = new WheelComponent(rootElementRef, data.length, {});
+    wheel.initialize(data);
+});
