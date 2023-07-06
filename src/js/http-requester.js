c727ef16a78002e018d9f0e30df28992997c6a78
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-07-06 10:58:51 +0300

    Refactored HttpRequest as a class extending Observable interface.

--- src/js/http-requester.js
+++ src/js/http-requester.js
@@ -8,280 +8,203 @@ import { Observable } from './observable.js';
  * @property {boolean} reportProgress 
  * @property {'arraybuffer'|'blob'|'json'|'text'} responseType 
  * @property {boolean} withCredentials 
+ * @property {boolean} async 
  */
 
-/**
- * Constructs HttpRequest observable object.
- * @constructor
- * @param {string} url 
- * @param {'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS'} method 
- * @param {any} data 
- * @param {Partial<HttpRequestOptions>} options 
- * @param {boolean} async 
- * @returns {Observable} 
- * 
- * @example <caption>Creating a GET request:</caption>
- * const getRequest = new HttpRequest('https://api.example.com/data', 'GET', {
- *   headers: {},
- *   params: {},
- *   observe: 'body',
- *   responseType: 'json',
- *   reportProgress: false,
- *   withCredentials: false
- * });
- *
- * @example <caption>Creating a POST request:</caption>
- * const data = { name: 'John', age: 25 };
- * const postRequest = new HttpRequest('https://api.example.com/data', data, 'POST', {
- *   headers: {},
- *   params: {},
- *   observe: 'body',
- *   responseType: 'json',
- *   reportProgress: false,
- *   withCredentials: false
- * });
- */
-export function HttpRequest(
-    url,
-    method = 'GET',
-    data = null,
-    options = {
-        headers: {},
-        params: {},
-        observe: 'body',
-        reportProgress: false,
-        responseType: 'json',
-        withCredentials: false
-    },
-    async = true
-) {
-    if (url == null || typeof url !== 'string') {
-        throw new TypeError('Url param should be a valid url string.');
-    }
 
-    const supportedHttpMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
-
-    if (method && !supportedHttpMethods.includes(method)) {
-        throw new TypeError(`Method param should be either ${supportedHttpMethods.join(', ')}.`);
-    }
-
-    method = method ?? 'GET';
-    options.headers = options.headers ?? {};
-    options.params = options.params ?? {};
-    options.observe = options.observe ?? 'body';
-    options.responseType = options.responseType ?? 'text';
-    options.reportProgress = options.reportProgress ?? false;
-    options.withCredentials = options.withCredentials ?? false;
+export class HttpRequest extends Observable {
+    #xhr;
+    #controller;
+    #url;
+    #method;
+    #data;
+    #options;
 
     /**
-     * Sends AJAX request.
+     * Constructs HttpRequest observable object.
+     * @constructor
      * @param {string} url 
      * @param {'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS'} method 
-     * @param {any} [data] 
-     * @param {Pick<HttpRequestOptions, "headers" | "responseType" | "withCredentials">} options 
-     * @param {boolean} async 
-     * @returns 
+     * @param {any} data 
+     * @param {Partial<HttpRequestOptions>} options  
+     * 
+     * @example <caption>Creating a GET request:</caption>
+     * const getRequest = new HttpRequest('https://api.example.com/data', 'GET', {
+     *   headers: {},
+     *   params: {},
+     *   observe: 'body',
+     *   responseType: 'json',
+     *   reportProgress: false,
+     *   withCredentials: false
+     * });
+     *
+     * @example <caption>Creating a POST request:</caption>
+     * const data = { name: 'John', age: 25 };
+     * const postRequest = new HttpRequest('https://api.example.com/data', data, 'POST', {
+     *   headers: {},
+     *   params: {},
+     *   observe: 'body',
+     *   responseType: 'json',
+     *   reportProgress: false,
+     *   withCredentials: false
+     * });
      */
-    function _sendAJAXRequest(url, method = 'GET', data = null, options, async = true) {
-        if (this.readyState === 0) {
-            this.open(method, url, async);
-            this.responseType = options.responseType;
-            this.withCredentials = options.withCredentials;
+    constructor(url, method = 'GET', data = null, options = {}) {
+        if (url == null || typeof url !== 'string') {
+            throw new TypeError('Url param should be a valid url string.');
+        }
+    
+        const supportedHttpMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
+    
+        if (method && !supportedHttpMethods.includes(method)) {
+            throw new TypeError(`Method param should be either ${supportedHttpMethods.join(', ')}.`);
+        }
+
+        method = method ?? 'GET';
+        options = typeof options !== 'object' ? {} : options;
+        options.headers = options.headers ?? {};
+        options.params = options.params ?? {};
+        options.observe = options.observe ?? 'body';
+        options.responseType = options.responseType ?? 'text';
+        options.reportProgress = options.reportProgress ?? false;
+        options.withCredentials = options.withCredentials ?? false;
+        options.async = options.async ?? true;
+
+        const xhr = new XMLHttpRequest();
+        const controller = new AbortController();
+
+        super((subscriber) => {
+            try {
+                // AbortController signal
+                const { signal } = this.#controller;
+                const options = this.#options;
+                // signal.addEventListener('abort', abortHandler);
+
+                if (options.observe === 'body' || options.observe === 'response') {
+                    xhr.addEventListener('load', () => {
+                        const result = options.observe === 'response' ? this.#response : this.#response.body;
+
+                        if (this.#response.status >= 200 && this.#response.status < 300) {
+                            subscriber.next(result);
+                        } else {
+                            subscriber.next(result);
+
+                            if (this.#response.status >= 400) {
+                                subscriber.error(new Error(`HTTP Error: ${this.#response.status} ${this.#response.statusText}`));
+                            }
+                        }
+
+                        if (xhr.readyState === 4) {
+                            // console.log('XHR completed');
+                            subscriber.complete();
+                        }
+                    }, { signal });
+
+                    xhr.addEventListener('error', () => {
+                        subscriber.error(new Error(`Network error`));
+                        subscriber.complete();
+                    }, { signal });
+
+                    xhr.addEventListener('timeout', () => {
+                        console.log(`Request to ${this.#url} timedout.`);
+                        subscriber.complete();
+                    }, { signal });
+
+                    xhr.addEventListener('abort', (ev) => {
+                        console.log(`Aborted request to ${this.#url}.`);
+                        subscriber.complete();
+                    }, { signal });
+                }
+
+                this.#sendAJAXRequest();
+
+                // Unsubscribe handler
+                return () => {
+                    this.#abortAJAXRequest();
+                    controller.abort('Unsubscribed from request event listener.');
+                };
+            } catch (err) {
+                subscriber.error(err);
+            }
+        });
+
+        this.#xhr = xhr;
+        this.#controller = controller;
+        this.#url = url;
+        this.#method = method;
+        this.#data = data;
+        this.#options = options;
+    }
+
+    /**
+     * Creates `GET` http request observable object.
+     * @param {string} url 
+     * @param {Partial<HttpRequestOptions>} options 
+     * 
+     * @example <caption>Creating a GET request:</caption>
+     * const getRequest = HttpRequest.get('https://api.example.com/data', {
+     *   headers: {},
+     *   params: {},
+     *   observe: 'body',
+     *   responseType: 'json',
+     *   reportProgress: false,
+     *   withCredentials: false
+     * });
+     */
+    static get(url, options = {}) {
+        return new this(url, 'GET', null, options);
+    }
+
+    get #response() {
+        return {
+            status: this.#xhr.status,
+            statusText: this.#xhr.statusText,
+            headers: this.#xhr.getAllResponseHeaders(),
+            body: this.#xhr.response
+        }
+    }
+
+    #sendAJAXRequest() {
+        const xhr = this.#xhr;
+        const url = this.#url;
+        const method = this.#method;
+        const data = this.#data;
+        const options = this.#options;
+
+        if (xhr && xhr.readyState === 0) {
+            xhr.open(method, url, options.async);
+            xhr.responseType = options.responseType;
+            xhr.withCredentials = options.withCredentials;
 
             const headers = options.headers && Object.entries(options.headers);
 
             if (headers) {
                 headers.forEach(([ name, content ]) => {
-                    this.setRequestHeader(name, content);
+                    xhr.setRequestHeader(name, content);
                 });
             }
 
             if (['POST', 'PUT', 'PATCH'].includes(method)) {
-                this.send(data);
+                xhr.send(data);
                 return;
             }
 
-            this.send();
+            xhr.send();
         }
     }
 
-    /**
-     * Aborts AJAX request.
-     * @param {string} url 
-     */
-    function _abortAJAXRequest(url) {
-        const states = ['OPENED', 'HEADERS_RECEIVED', 'LOADING'];
-        states.map(state => this[state]);
-        const abortAllowed = states.includes(this.readyState);
-        // console.log(`Request to '${url}' with state ${this.readyState} is ${abortAllowed ? '' : 'not'} abortabale.`);
-        if (abortAllowed) {
-            this.abort();
+    #abortAJAXRequest() {
+        const xhr = this.#xhr;
+        const url = this.#url;
+
+        if (xhr) {
+            const states = ['OPENED', 'HEADERS_RECEIVED', 'LOADING'];
+            states.map(state => xhr[state]);
+            const abortAllowed = states.includes(xhr.readyState);
+            // console.log(`Request to '${url}' with state ${xhr.readyState} is ${abortAllowed ? '' : 'not'} abortabale.`);
+            if (abortAllowed) {
+                xhr.abort();
+            }
         }
     }
-
-    /**
-     * A wrapper method for invoking the 
-     * `_abortAJAXRequest` internally with the 
-     * current `xhr` as `this`.
-     * @param {XMLHttpRequest} xhr 
-     */
-    let abortAJAXRequest = function (xhr) {
-        _abortAJAXRequest.call(xhr, url);
-    };
-
-    /**
-     * A wrapper method for invoking the 
-     * `_sendAJAXRequest` internnally with the 
-     * current `xhr` as `this`.
-     * @param {XMLHttpRequest} xhr 
-     */
-    let sendAJAXRequest = function (xhr) {
-        _sendAJAXRequest.call(xhr, url, method, data, options, async);
-    };
-
-    return Reflect.construct(Observable, [function (subscriber) {
-
-        // Initialize XHR and Controller
-        const controller = new AbortController();
-        const { signal } = controller;
-        const xhr = new XMLHttpRequest();
-    
-        // Add event listeners
-        const abortHandler = () => {
-            console.log('Unsubscribed from request event listener.');
-        };
-        signal.addEventListener('abort', abortHandler);
-        
-        if (options.observe === 'events') {
-
-            xhr.addEventListener('loadstart', (ev) => {
-                subscriber.next(ev);
-            }, { signal });
-
-            xhr.addEventListener('load', (ev) => {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    subscriber.next(ev);
-                }
-            }, { signal });
-
-            xhr.addEventListener('loadend', (ev) => {
-                subscriber.next(ev);
-                subscriber.complete();
-            }, { signal });
-
-            if (options.reportProgress) {
-
-                xhr.addEventListener('progress', (ev) => {
-                    if (ev.lengthComputable) {
-                        console.log(`Loaded ${ev.loaded} of ${ev.total}`);
-                    } else {
-                        console.log(`Loaded ${ev.loaded}`);
-                    }
-
-                    subscriber.next(ev);
-                }, { signal });
-
-            }
-
-            xhr.addEventListener('abort', (ev) => {
-                console.log(`Aborted ongoing request to ${this.url}`);
-                subscriber.next(ev);
-                subscriber.complete();
-            }, { signal });
-
-            xhr.addEventListener('error', (ev) => {
-                console.log(`Error occurred on request to ${this.url}`);
-                subscriber.next(ev);
-                subscriber.complete();
-            }, { signal });
-
-            xhr.addEventListener('timeout', (ev) => {
-                console.log(`Request to ${this.url} timedout`);
-                subscriber.next(ev);
-                subscriber.complete();
-            })
-
-        } else {
-
-            xhr.addEventListener('load', (ev) => {
-                const response = {
-                    status: xhr.status,
-                    statusText: xhr.statusText,
-                    headers: xhr.getAllResponseHeaders(),
-                    body: xhr.response
-                };
-
-                const result = options.observe === 'response' ? response : response.body;
-
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    subscriber.next(result);
-                } else {
-                    subscriber.next(result);
-
-                    if (xhr.status >= 400) {
-                        subscriber.error(new Error(`HTTP Error: ${xhr.status} ${xhr.statusText}`));
-                    }
-                }
-
-                if (xhr.readyState === 4) {
-                    // console.log('XHR ready state from load event handler', xhr.readyState);
-                    subscriber.complete();
-                }
-            }, { signal });
-
-            xhr.addEventListener('error', (ev) => {
-                subscriber.error(new Error(`HTTP Error: ${xhr.status} ${xhr.statusText}`));
-                subscriber.complete();
-            }, { signal });
-
-            xhr.addEventListener('abort', (ev) => {
-                console.log(`Aborted ongoing request to ${this.url}`);
-                subscriber.complete();
-            }, { signal });
-
-        }
-
-        sendAJAXRequest(xhr);
-
-        // Unsubscribe handler
-        return () => {
-            abortAJAXRequest(xhr);
-            controller.abort('Unsubscribed from request event listener.');
-            if (signal.aborted) {
-                signal.removeEventListener('abort', abortHandler);
-            }
-        };
-    }]);
-}
-
-/**
- * Creates `GET` http request observable object.
- * @param {string} url 
- * @param {Partial<HttpRequestOptions>} options 
- * @param {boolean} async 
- * @returns {Observable} 
- * 
- * @example <caption>Creating a GET request:</caption>
- * const getRequest = HttpRequest.get('https://api.example.com/data', {
- *   headers: {},
- *   params: {},
- *   observe: 'body',
- *   responseType: 'json',
- *   reportProgress: false,
- *   withCredentials: false
- * });
- */
-HttpRequest.get = function (
-    url,
-    options = {
-        headers: {},
-        observe: 'body',
-        params: {},
-        reportProgress: false,
-        responseType: 'json',
-        withCredentials: false
-    },
-    async = true
-) {
-    return HttpRequest(url, 'GET', null, options, async);
 }
