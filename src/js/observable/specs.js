e5390e46ebc05969ac2dd9453b8c595bc072f5e7
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-07-12 00:53:25 +0300

    Added appropriate tests and examples to validate the behaviour of the `concat` method.

--- src/js/observable/specs.js
+++ src/js/observable/specs.js
@@ -1,18 +1,24 @@
-import { concat, interval, range, take } from './index.js';
+import { Observable, concat, interval, range, take } from './index.js';
 
-const timer1 = interval(1000).pipe(take(6));
-const timer2 = interval(1000).pipe(take(6));
-const sequence = range(1, 5);
-const result = concat(sequence);
-
-const subscription = result.subscribe({
-    next: x => console.log(x),
-    complete: () => {
-        console.log('Complete handler');
-    }
+const observable1 = new Observable((observer) => {
+    observer.next('Observable 1 - Value 1');
+    observer.next('Observable 1 - Value 2');
+    observer.complete();
 });
 
-let timerId = globalThis.setTimeout(() => {
-    globalThis.clearTimeout(timerId);
-    subscription.unsubscribe();
-}, 3000);
+const observable2 = new Observable((observer) => {
+    observer.next('Observable 2 - Value 1');
+    observer.next('Observable 2 - Value 2');
+    observer.complete();
+});
+
+const timer1 = interval(1000).pipe(take(10));
+const timer2 = interval(2000).pipe(take(6));
+const timer3 = interval(500).pipe(take(10));
+const sequence = range(1, 10);
+
+const result = concat(observable1, timer1, timer2, timer3, sequence, timer1, observable2);
+result.subscribe({
+    next: x => console.log(x),
+    complete: () => console.log('...and it is done!')
+});
