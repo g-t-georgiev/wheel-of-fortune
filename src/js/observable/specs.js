592e0e7eb7d7e4b670217e5be900601d1de81e07
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-07-12 12:19:13 +0300

    Added test to validate merge observable behaviour.

--- src/js/observable/specs.js
+++ src/js/observable/specs.js
@@ -1,24 +1,5 @@
-import { Observable, concat, interval, range, take } from './index.js';
+import { merge, interval, take, tap, map } from './index.js';
 
-const observable1 = new Observable((observer) => {
-    observer.next('Observable 1 - Value 1');
-    observer.next('Observable 1 - Value 2');
-    observer.complete();
-});
-
-const observable2 = new Observable((observer) => {
-    observer.next('Observable 2 - Value 1');
-    observer.next('Observable 2 - Value 2');
-    observer.complete();
-});
-
-const timer1 = interval(1000).pipe(take(10));
-const timer2 = interval(2000).pipe(take(6));
-const timer3 = interval(500).pipe(take(10));
-const sequence = range(1, 10);
-
-const result = concat(observable1, timer1, timer2, timer3, sequence, timer1, observable2);
-result.subscribe({
-    next: x => console.log(x),
-    complete: () => console.log('...and it is done!')
-});
+const a = interval(500).pipe(map((v) => 'a' + v), take(3));
+const b = interval(1000).pipe(map((v) => 'b' + v), take(3));
+merge(a, b).subscribe((value) => console.log(value));
