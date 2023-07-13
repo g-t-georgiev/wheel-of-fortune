a49e26f764537080391f1820187301d2bc3398dc
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2023-07-13 10:28:49 +0300

    Added tests for `concatAll` operator.

--- src/js/observable/specs.js
+++ src/js/observable/specs.js
@@ -1,16 +1,8 @@
-import { interval, take, map, merge } from './index.js';
+import { interval, take, concatAll, fromEvent, map } from './index.js';
 
-const timer1 = interval(1000).pipe(take(10), map(v => `A - ${v}`));
-const timer2 = interval(2000).pipe(take(6), map(v => `B - ${v}`));
-const timer3 = interval(500).pipe(take(10), map(v => `C - ${v}`));
- 
-const concurrent = 2; // the argument
-const merged = merge(timer1, timer2, timer3, concurrent);
-merged.subscribe({
-    next(x) {
-        console.log(x);
-    },
-    complete() {
-        console.log('Completed');
-    }
-});
+const clicks = fromEvent(document, 'click');
+const higherOrder = clicks.pipe(
+  map(() => interval(1000).pipe(take(4)))
+);
+const firstOrder = higherOrder.pipe(concatAll());
+firstOrder.subscribe(x => console.log(x));
